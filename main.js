e27bd811bf281620435ebc36c9be89eb9966cc37
todun <todun@users.noreply.github.com>
2018-09-10 21:39:59 +0000

    added docs

--- main.js
+++ main.js
@@ -6,7 +6,12 @@ const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
 
-// takes an array, and returns one of the items stored inside the array at random.
+/**
+ * takes an array, and returns one of the items stored inside the array at random.
+ *
+ * @param array
+ * @returns {*}
+ */
 function randomValueFromArray(array) {
     return array[Math.floor(Math.random() * array.length)];
 }
@@ -25,6 +30,9 @@ const insertZ = ["spontaneously combusted", "melted into a puddle on the sidewal
 
 randomize.addEventListener('click', result);
 
+/**
+ * update view with output
+ */
 function result() {
     let newStory = storyText;
     const xItem = randomValueFromArray(insertX);
@@ -53,14 +61,31 @@ function result() {
     story.style.visibility = 'visible';
 }
 
+/**
+ * convert between fahrenheit and celsius
+ * @param fahrenheit
+ * @returns {number}
+ */
 function fahrenheit2celsius(fahrenheit) {
     return (fahrenheit - 32) / 1.8;
 }
 
+/**
+ * convert pounds to stone
+ * @param pounds
+ * @returns {number}
+ */
 function pounds2stone(pounds) {
     return pounds * 0.071429;
 }
 
+/**
+ * search over entire document
+ * @param input string
+ * @param current
+ * @param replacement text
+ * @returns {string}
+ */
 function replaceTwice(input, current, replacement) {
     return input.replace(current, replacement).replace(current, replacement);
 }
